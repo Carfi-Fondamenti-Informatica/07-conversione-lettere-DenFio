2d7c8fb584e9d4bab3bde14d06888f10a82d5991
Dennis Fiorenza <115047044+DenFio@users.noreply.github.com>
2022-12-09 11:18:12 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,4 +1,30 @@
 #include "lib.h"
 unsigned char ris;
 
-bool conv(char c) 
+bool conv(char c) {
+     if (c>="a" && c<="z") {
+        ris=c-32;
+        return true;
+     }else if (c>="A" && c<="Z") {
+          ris =c+32;
+          return true;
+     }else{
+          return false;
+     }
+}
+     
+     
+     
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
